05d23e5d2d42925cc99b75b71b44d710f46f066b
sajjad yosefi <yosefi1988@gmail.com>
2020-08-16 00:38:24 -0700

    26 mordad 5

--- accountauthenticator/src/main/java/ir/sajjadyosefi/accountauthenticator/authentication/ServerAuthenticate.java
+++
@@ -1,9 +0,0 @@
-package ir.sajjadyosefi.accountauthenticator.authentication;
-
-import ir.sajjadyosefi.accountauthenticator.model.LoginRequest;
-import ir.sajjadyosefi.accountauthenticator.model.User;
-
-public interface ServerAuthenticate {
-    public User userSignUp(final LoginRequest loginRequest) throws Exception;
-    public User userSignIn(final LoginRequest loginRequest) throws Exception;
-}
--- accountauthenticator2/src/main/java/ir/sajjadyosefi/accountauthenticator/authentication/ServerAuthenticate.java
+++
@@ -1,6 +0,0 @@
-package ir.sajjadyosefi.accountauthenticator.authentication;
-
-public interface ServerAuthenticate {
-    public String userSignUp(final String name, final String email, final String pass, String authType) throws Exception;
-    public String userSignIn(final String user, final String pass, String authType) throws Exception;
-}
